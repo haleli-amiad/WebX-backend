7efea91bd1c0321d2aa1d78c271de420f1f51a8c
Nehoray Ilani <neho12@gmail.com>
2020-12-06 00:49:46 +0200

    first commit

---
+++ public/precache-manifest.06002c6f0b354067d66cfa392529f484.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1df4f601ad2cb506db7",
+    "url": "/css/app.4de9d7f0.css"
+  },
+  {
+    "revision": "6aa49aa08938126398bc",
+    "url": "/css/chunk-vendors.95218efd.css"
+  },
+  {
+    "revision": "28195c14e6a271f42ca0994fac4fb0e4",
+    "url": "/fonts/AbrilFatface-Regular.28195c14.ttf"
+  },
+  {
+    "revision": "be4ebc30e0ddc2634a2a2074af751df2",
+    "url": "/fonts/AlmondNougat.be4ebc30.ttf"
+  },
+  {
+    "revision": "3cb6e41d90b52657b135d714933cd824",
+    "url": "/fonts/Assistant-Bold.3cb6e41d.ttf"
+  },
+  {
+    "revision": "4c776b4332f404a553812af27853395b",
+    "url": "/fonts/Assistant-ExtraLight.4c776b43.ttf"
+  },
+  {
+    "revision": "3670aa493ee09e92c7b8e1e7c2f5b441",
+    "url": "/fonts/CaviarDreams.3670aa49.ttf"
+  },
+  {
+    "revision": "0899368ee7bce0b8640ea40e3149574d",
+    "url": "/fonts/ChampagneLimousines.0899368e.ttf"
+  },
+  {
+    "revision": "62f10d46561ed0b911d42b7510d62d74",
+    "url": "/fonts/CrimsonText-Regular.62f10d46.ttf"
+  },
+  {
+    "revision": "b25abc8ada7a34002951429464b71681",
+    "url": "/fonts/FUNCYKIDS.b25abc8a.ttf"
+  },
+  {
+    "revision": "9122299e476671f970e1670bd7b900c8",
+    "url": "/fonts/FrederickatheGreat-Regular.9122299e.ttf"
+  },
+  {
+    "revision": "3b5635ac5d39c28200bf3993d90d09c7",
+    "url": "/fonts/Raleway-VariableFont_wght.3b5635ac.ttf"
+  },
+  {
+    "revision": "77fa00996ecb4104c7880b8749c7c4e0",
+    "url": "/fonts/Righteous-Regular.77fa0099.ttf"
+  },
+  {
+    "revision": "fa8ab495d494eccb28f4431f054ddbd4",
+    "url": "/fonts/RobotoMono-Light.fa8ab495.ttf"
+  },
+  {
+    "revision": "e5ca8c0ac474df46fe45840707a0c483",
+    "url": "/fonts/RobotoMono-Regular.e5ca8c0a.ttf"
+  },
+  {
+    "revision": "85259c0a972c45b7da0f4e34869191cd",
+    "url": "/fonts/RomantikaHidupRegular.85259c0a.ttf"
+  },
+  {
+    "revision": "cd619a4f068dc66cc6e58fe0a91a8a34",
+    "url": "/fonts/Rubik-Regular.cd619a4f.ttf"
+  },
+  {
+    "revision": "66b0e223824fd123ab079b60da594ea7",
+    "url": "/fonts/Sacramento-Regular.66b0e223.ttf"
+  },
+  {
+    "revision": "81cd217e4a8160a930c6d5fb8d1e8e82",
+    "url": "/fonts/SourceSansPro-Light.81cd217e.ttf"
+  },
+  {
+    "revision": "c1678b46f7dd3f50ceac94ed4e0ad01a",
+    "url": "/fonts/SourceSansPro-Regular.c1678b46.ttf"
+  },
+  {
+    "revision": "36a1c9e191f6087449eb31eb743aa2c0",
+    "url": "/fonts/SundayMorning.36a1c9e1.ttf"
+  },
+  {
+    "revision": "e5f127705602659ddbec6b0cce391ca4",
+    "url": "/fonts/caudex.regular.e5f12770.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "3f4fe233eedc22b2aa12a0bee204a20d",
+    "url": "/fonts/kulo.3f4fe233.otf"
+  },
+  {
+    "revision": "4236d709fa7368fcf3731fe47d68754f",
+    "url": "/fonts/pumpkins.4236d709.otf"
+  },
+  {
+    "revision": "ec1e618335b4a217531f09001fe87db6",
+    "url": "/img/drag002.ec1e6183.png"
+  },
+  {
+    "revision": "4311d275d7e2bceed43ff49a9fe747cb",
+    "url": "/img/logo2.4311d275.png"
+  },
+  {
+    "revision": "263a20d0023e7d8f40d24b8570d0ad37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1df4f601ad2cb506db7",
+    "url": "/js/app.e214c159.js"
+  },
+  {
+    "revision": "6aa49aa08938126398bc",
+    "url": "/js/chunk-vendors.46586b26.js"
+  },
+  {
+    "revision": "395f63d1ae5d0faede3c6d9578270a24",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "29cc974ab4a5b20ad530f226918c7d43",
+    "url": "/media/video.29cc974a.mp4"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
